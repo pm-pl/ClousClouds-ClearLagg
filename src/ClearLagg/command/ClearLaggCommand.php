88a4905ee1b84a15141ab156b34ad9457903b357
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-06-04 19:44:53 +0700

    Create ClearLaggCommand.php

---
+++ src/ClearLagg/command/ClearLaggCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ClearLagg\command;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use pocketmine\plugin\Plugin;
+
+class ClearLaggCommand extends Command {
+
+    private Plugin $plugin;
+
+    public function __construct(Plugin $plugin) {
+        parent::__construct("clearlagg", "Clears all dropped items", "/clearlagg", []);
+        $this->plugin = $plugin;
+        $this->setPermission("clearlagg.use");
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
+        if (!$this->testPermission($sender)) {
+            return false;
+        }
+        $this->plugin->getClearLagManager()->clearLag();
+        $sender->sendMessage("All dropped items have been cleared.");
+        return true;
+    }
+}
