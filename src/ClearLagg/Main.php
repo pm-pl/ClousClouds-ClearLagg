21e959b6cb33c8afcf15fd15c1e059cf13ef4790
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-06-15 06:03:02 +0700

    Update Main.php

--- src/ClearLagg/Main.php
+++ src/ClearLagg/Main.php
@@ -3,34 +3,34 @@
 namespace ClearLagg;
 
 use pocketmine\plugin\PluginBase;
-use ClearLagg\task\ClearLaggTask;
+use pocketmine\event\Listener;
+use ClearLagg\command\ClearLaggCommand;
+use ClearLagg\listener\EventListener;
 use ClearLagg\manager\ClearLaggManager;
 use ClearLagg\manager\ConfigManager;
 use ClearLagg\manager\StatsManager;
-use ClearLagg\listener\EventListener;
-use ClearLagg\command\ClearLaggCommand;
 
-class Main extends PluginBase {
+class Main extends PluginBase implements Listener {
 
+    /** @var ClearLaggManager */
     private ClearLaggManager $clearLaggManager;
+
+    /** @var ConfigManager */
     private ConfigManager $configManager;
+
+    /** @var StatsManager */
     private StatsManager $statsManager;
 
     public function onEnable(): void {
+        $this->saveDefaultConfig();
         $this->configManager = new ConfigManager($this);
         $this->clearLaggManager = new ClearLaggManager($this);
         $this->statsManager = new StatsManager();
 
-        foreach ($this->configManager->getConfig()->get("worlds", []) as $worldName => $settings) {
-            $interval = $settings["interval"] ?? 300;
-            $this->getScheduler()->scheduleRepeatingTask(
-                new ClearLaggTask($this),
-                20 * $interval
-            );
-        }
-
         $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
         $this->getServer()->getCommandMap()->register("clearlagg", new ClearLaggCommand($this));
+
+        $this->getScheduler()->scheduleRepeatingTask(new AutoClearTask($this), 20 * $this->getConfig()->get("auto-clear-interval"));
     }
 
     public function getClearLaggManager(): ClearLaggManager {
