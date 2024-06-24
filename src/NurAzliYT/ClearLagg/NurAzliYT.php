6eec90f08cce81bba6bdb35ee37c2e0ea772ebc9
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2024-06-24 17:30:13 +0700

    Create NurAzliYT.php

---
+++ src/NurAzliYT/ClearLagg/NurAzliYT.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace NurAzliYT\ClearLagg;
+
+use NurAzliYT\ClearLagg\Main;
+
+final class NurAzliYT extends Main{
+  /** Copyright 2024 NurAzliYT */
+  /** Anyone who plagiarizes without our permission will violate state laws */
+   }
+}
