b22fc1571d774d9868c162f8d99f46d9eab752a8
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2024-06-24 16:29:12 +0700

    Update Main.php

--- src/NurAzliYT/ClearLagg/Main.php
+++ src/NurAzliYT/ClearLagg/Main.php
@@ -3,46 +3,61 @@
 namespace NurAzliYT\ClearLagg;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
-use NurAzliYT\ClearLagg\command\ClearLaggCommand;
-use NurAzliYT\ClearLagg\listener\EventListener;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\Config;
 use NurAzliYT\ClearLagg\manager\ClearLaggManager;
-use NurAzliYT\ClearLagg\manager\ConfigManager;
 use NurAzliYT\ClearLagg\manager\StatsManager;
-use NurAzliYT\ClearLagg\task\AutoClearTask;
+use NurAzliYT\ClearLagg\command\ClearLaggCommand;
+use NurAzliYT\ClearLagg\command\subcommands\StatsCommand;
 
-class Main extends PluginBase implements Listener {
+class Main extends PluginBase {
 
-    /** @var ClearLaggManager */
-    private ClearLaggManager $clearLaggManager;
-
-    /** @var ConfigManager */
-    private ConfigManager $configManager;
-
-    /** @var StatsManager */
-    private StatsManager $statsManager;
+    private Config $config;
 
     public function onEnable(): void {
         $this->saveDefaultConfig();
-        $this->configManager = new ConfigManager($this);
-        $this->clearLaggManager = new ClearLaggManager($this);
-        $this->statsManager = new StatsManager();
-
-        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
-        $this->getServer()->getCommandMap()->register("clearlagg", new ClearLaggCommand($this));
-
-        $this->getScheduler()->scheduleRepeatingTask(new AutoClearTask($this), 20 * $this->getConfig()->get("auto-clear-interval"));
+        $this->config = $this->getConfig();
+        $this->getLogger()->info("ClearLagg plugin enabled!");
+        $this->registerCommands();
+        $this->startClearlagg();
     }
 
-    public function getClearLaggManager(): ClearLaggManager {
-        return $this->clearLaggManager;
+    private function registerCommands() {
+        $commandMap = $this->getServer()->getCommandMap();
+        $commandMap->register("clearlagg", new ClearLaggCommand($this));
+        $commandMap->register("clearlaggstats", new StatsCommand($this));
     }
 
-    public function getConfigManager(): ConfigManager {
-        return $this->configManager;
+    private function startClearlagg() {
+        $interval = $this->config->get("auto-clear-interval", 300);  // Default 300 detik (5 menit)
+        $this->getScheduler()->scheduleRepeatingTask(new ClearLaggManager($this), 20 * $interval);
     }
 
-    public function getStatsManager(): StatsManager {
-        return $this->statsManager;
+    public function getClearLaggConfig(): Config {
+        return $this->config;
+    }
+
+    public function notifyPlayers(string $message, int $countdown) {
+        $task = new class($this, $message, $countdown) extends \pocketmine\scheduler\Task {
+            private Main $plugin;
+            private string $message;
+            private int $countdown;
+
+            public function __construct(Main $plugin, string $message, int $countdown) {
+                $this->plugin = $plugin;
+                $this->message = $message;
+                $this->countdown = $countdown;
+            }
+
+            public function onRun(): void {
+                $this->plugin->getServer()->broadcastMessage($this->message . " in " . $this->countdown . " seconds!");
+                if ($this->countdown <= 0) {
+                    $this->plugin->getScheduler()->cancelTask($this->getTaskId());
+                }
+                $this->countdown -= 10;
+            }
+        };
+        $this->getScheduler()->scheduleRepeatingTask($task, 20 * 10);
     }
 }
