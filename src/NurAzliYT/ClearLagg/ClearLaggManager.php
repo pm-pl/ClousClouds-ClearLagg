2390ccda863b3ad38c82b9eaeb49d680882a7f14
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2024-06-25 12:26:32 +0700

    Create ClearLaggManager.php

---
+++ src/NurAzliYT/ClearLagg/ClearLaggManager.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace NurAzliYT\ClearLagg;
+
+use pocketmine\Server;
+use pocketmine\entity\object\ItemEntity;
+use pocketmine\utils\TextFormat;
+
+class ClearLaggManager {
+
+    private $plugin;
+    private $clearMessage;
+    private $warningMessage;
+
+    public function __construct(Main $plugin) {
+        $this->plugin = $plugin;
+        $config = $plugin->getConfig();
+        $this->clearMessage = TextFormat::colorize($config->get("clear-message", "§aGarbage collected correctly."));
+        $this->warningMessage = TextFormat::colorize($config->get("warning-message", "§cPicking up trash in {time}..."));
+    }
+
+    public function clearItems(): void {
+        $itemsCleared = 0;
+        foreach (Server::getInstance()->getWorldManager()->getWorlds() as $world) {
+            foreach ($world->getEntities() as $entity) {
+                if ($entity instanceof ItemEntity) {
+                    $entity->flagForDespawn();
+                    $itemsCleared++;
+                }
+            }
+        }
+        $this->plugin->getStatsManager()->incrementItemsCleared($itemsCleared);
+        Server::getInstance()->broadcastMessage($this->clearMessage);
+    }
+
+    public function getWarningMessage(int $timeRemaining): string {
+        return str_replace("{time}", (string)$timeRemaining, $this->warningMessage);
+    }
+}
