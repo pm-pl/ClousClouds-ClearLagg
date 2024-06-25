367ffee626d665234dee7af34080c7e104c24dc4
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2024-06-25 12:30:30 +0700

    Create StatsCommand.php

---
+++ src/NurAzliYT/ClearLagg/command/subcommands/StatsCommand.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace NurAzliYT\ClearLagg\subcommands;
+
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat;
+use NurAzliYT\ClearLagg\Main;
+
+class StatsCommand {
+
+    private $plugin;
+
+    public function __construct(Main $plugin) {
+        $this->plugin = $plugin;
+    }
+
+    public function execute(CommandSender $sender): void {
+        $itemsCleared = $this->plugin->getStatsManager()->getItemsCleared();
+        $sender->sendMessage(TextFormat::GREEN . "Total items cleared: " . $itemsCleared);
+    }
+}
