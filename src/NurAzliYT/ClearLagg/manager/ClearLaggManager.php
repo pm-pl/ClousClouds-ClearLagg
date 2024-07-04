fedec9902f47a739f2a3299b116cb1c1bf8fa351
NurAzli <144025939+NurAzli@users.noreply.github.com>
2024-07-04 19:59:03 +0700

    Update for supported new config versions in ClearLaggManager.php files

--- src/NurAzliYT/ClearLagg/manager/ClearLaggManager.php
+++ src/NurAzliYT/ClearLagg/manager/ClearLaggManager.php
@@ -1,40 +1,68 @@
 <?php
 
-namespace NurAzliYT\ClearLagg\manager;
+namespace NurAzliYT\ClearLagg;
 
-use NurAzliYT\ClearLagg\Main;
+use pocketmine\scheduler\ClosureTask;
 use pocketmine\Server;
 use pocketmine\entity\object\ItemEntity;
-use pocketmine\utils\TextFormat;
 
 class ClearLaggManager {
 
     private $plugin;
+    private $clearInterval;
     private $clearMessage;
     private $warningMessage;
+    private $broadcastInterval;
+    private $broadcastMessage;
+    private $timeRemaining;
 
     public function __construct(Main $plugin) {
         $this->plugin = $plugin;
-        $config = $plugin->getConfig();
-        $this->clearMessage = TextFormat::colorize($config->get("clear-message", "§aGarbage collected correctly."));
-        $this->warningMessage = TextFormat::colorize($config->get("warning-message", "§cPicking up trash in {time}..."));
+    }
+
+    public function init(): void {
+        $config = $this->plugin->getConfig();
+        $this->clearInterval = $config->get("clear-interval", 300);
+        $this->clearMessage = $config->get("clear-message", "§aGarbage collected correctly.");
+        $this->warningMessage = $config->get("warning-message", "§cPicking up trash in {time}...");
+        $this->broadcastInterval = $config->get("broadcast-interval", 15);
+        $this->broadcastMessage = $config->get("broadcast-message", "§bThe items will be deleted in {time} seconds.");
+        $this->timeRemaining = $config->getNested("notify-players.countdown", 299);
+
+        $this->plugin->getScheduler()->scheduleRepeatingTask(new ClosureTask(function(): void {
+            $this->onTick();
+        }), 20);
+
+        $this->plugin->getScheduler()->scheduleRepeatingTask(new ClosureTask(function(): void {
+            $this->broadcastTime();
+        }), $this->broadcastInterval * 20);
+    }
+
+    private function onTick(): void {
+        if ($this->timeRemaining <= 5 && $this->timeRemaining > 0) {
+            Server::getInstance()->broadcastMessage(str_replace("{time}", (string)$this->timeRemaining, $this->warningMessage));
+        }
+
+        if ($this->timeRemaining <= 0) {
+            $this->clearItems();
+            $this->timeRemaining = $this->clearInterval;
+        } else {
+            $this->timeRemaining--;
+        }
     }
 
     public function clearItems(): void {
-        $itemsCleared = 0;
         foreach (Server::getInstance()->getWorldManager()->getWorlds() as $world) {
             foreach ($world->getEntities() as $entity) {
                 if ($entity instanceof ItemEntity) {
                     $entity->flagForDespawn();
-                    $itemsCleared++;
                 }
             }
         }
-        $this->plugin->getStatsManager()->incrementItemsCleared($itemsCleared);
         Server::getInstance()->broadcastMessage($this->clearMessage);
     }
 
-    public function getWarningMessage(int $timeRemaining): string {
-        return str_replace("{time}", (string)$timeRemaining, $this->warningMessage);
+    private function broadcastTime(): void {
+        Server::getInstance()->broadcastMessage(str_replace("{time}", (string)$this->timeRemaining, $this->broadcastMessage));
     }
 }
