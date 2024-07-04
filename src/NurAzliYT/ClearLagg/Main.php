f566204bf8e094220cc4757b81f3dbd98c264849
NurAzli <144025939+NurAzli@users.noreply.github.com>
2024-07-04 19:55:24 +0700

    Update Main.php

--- src/NurAzliYT/ClearLagg/Main.php
+++ src/NurAzliYT/ClearLagg/Main.php
@@ -5,11 +5,8 @@ namespace NurAzliYT\ClearLagg;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\utils\Config;
+use pocketmine\command\PluginCommand;
 use pocketmine\utils\TextFormat;
-use pocketmine\scheduler\ClosureTask;
-use pocketmine\scheduler\TaskHandler;
-use pocketmine\Server;
 use NurAzliYT\ClearLagg\manager\ClearLaggManager;
 use NurAzliYT\ClearLagg\manager\StatsManager;
 use NurAzliYT\ClearLagg\command\ClearLaggCommand;
@@ -19,34 +16,27 @@ class Main extends PluginBase {
 
     private $clearLaggManager;
     private $statsManager;
-    private $clearTaskHandler;
-    private $broadcastTaskHandler;
-    private $timeRemaining;
 
     public function onEnable(): void {
         $this->saveDefaultConfig();
         $this->clearLaggManager = new ClearLaggManager($this);
         $this->statsManager = new StatsManager($this);
 
-        $this->timeRemaining = $this->getConfig()->get("auto-clear-interval", 300);
+        $this->clearLaggManager->init();
 
-        $this->clearTaskHandler = $this->getScheduler()->scheduleRepeatingTask(new ClosureTask(function(): void {
-            $this->onTick();
-        }), 20);
+        $this->registerCommands();
+    }
 
-        $broadcastInterval = $this->getConfig()->get("broadcast-interval", 15);
-        $this->broadcastTaskHandler = $this->getScheduler()->scheduleRepeatingTask(new ClosureTask(function(): void {
-            $this->broadcastTime();
-        }), $broadcastInterval * 20);
+    private function registerCommands(): void {
+        $clearLaggCommand = new ClearLaggCommand($this);
+        $this->getServer()->getCommandMap()->register("clearlagg", $clearLaggCommand);
+
+        $statsCommand = new StatsCommand($this);
+        $this->getServer()->getCommandMap()->register("clearlaggstats", $statsCommand);
     }
 
     public function onDisable(): void {
-        if ($this->clearTaskHandler instanceof TaskHandler) {
-            $this->clearTaskHandler->cancel();
-        }
-        if ($this->broadcastTaskHandler instanceof TaskHandler) {
-            $this->broadcastTaskHandler->cancel();
-        }
+        $this->clearLaggManager->shutdown();
     }
 
     public function getClearLaggManager(): ClearLaggManager {
@@ -55,36 +45,5 @@ class Main extends PluginBase {
 
     public function getStatsManager(): StatsManager {
         return $this->statsManager;
-    }
-
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
-        if (strtolower($command->getName()) === "clearlagg") {
-            if (count($args) > 0 && strtolower($args[0]) === "stats") {
-                (new StatsCommand($this))->execute($sender);
-            } else {
-                $this->clearLaggManager->clearItems();
-                $sender->sendMessage(TextFormat::GREEN . "Items cleared!");
-            }
-            return true;
-        }
-        return false;
-    }
-
-    private function onTick(): void {
-        if ($this->timeRemaining <= 5 && $this->timeRemaining > 0) {
-            $this->getServer()->broadcastMessage($this->clearLaggManager->getWarningMessage($this->timeRemaining));
-        }
-
-        if ($this->timeRemaining <= 0) {
-            $this->clearLaggManager->clearItems();
-            $this->statsManager->incrementItemsCleared();
-            $this->timeRemaining = $this->getConfig()->get("auto-clear-interval", 300);
-        } else {
-            $this->timeRemaining--;
-        }
-    }
-
-    private function broadcastTime(): void {
-        $this->getServer()->broadcastMessage(str_replace("{time}", (string)$this->timeRemaining, $this->getConfig()->get("broadcast-message", "§bThe items will be deleted in {time} seconds.")));
     }
 }
