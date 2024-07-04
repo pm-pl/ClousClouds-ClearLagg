a53c97d8c27ee92b59a5526bf547a3651aef2533
NurAzli <144025939+NurAzli@users.noreply.github.com>
2024-07-04 20:07:47 +0700

    Update StatsCommand.php

--- src/NurAzliYT/ClearLagg/command/subcommands/StatsCommand.php
+++ src/NurAzliYT/ClearLagg/command/subcommands/StatsCommand.php
@@ -1,21 +1,34 @@
 <?php
 
-namespace NurAzliYT\ClearLagg\command\subcommands;
+namespace NurAzliYT\ClearLagg;
 
+use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\utils\TextFormat;
+use pocketmine\plugin\PluginOwned;
+use pocketmine\plugin\PluginOwnedTrait;
 use NurAzliYT\ClearLagg\Main;
 
-class StatsCommand {
+class StatsCommand extends Command implements PluginOwned {
+    use PluginOwnedTrait;
 
     private $plugin;
 
     public function __construct(Main $plugin) {
+        parent::__construct("clearlaggstats", "View ClearLagg statistics", "/clearlagg stats", []);
         $this->plugin = $plugin;
+        $this->setPermission("clearlagg.stats.command");
     }
 
-    public function execute(CommandSender $sender): void {
-        $itemsCleared = $this->plugin->getStatsManager()->getItemsCleared();
-        $sender->sendMessage(TextFormat::GREEN . "Total items cleared: " . $itemsCleared);
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
+        if (!$this->testPermission($sender)) {
+            return false;
+        }
+
+        $this->plugin->getStatsManager()->sendStats($sender);
+        return true;
+    }
+
+    public function getOwningPlugin(): Main {
+        return $this->plugin;
     }
 }
