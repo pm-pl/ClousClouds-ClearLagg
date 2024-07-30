5667641d1e212f1ab5d605a7b5ae0daa132893b0
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2024-07-31 04:59:52 +0700

    Update NurAzliYT.php

--- src/NurAzliYT/ClearLagg/NurAzliYT.php
+++ src/NurAzliYT/ClearLagg/NurAzliYT.php
@@ -5,6 +5,7 @@ namespace NurAzliYT\ClearLagg;
 use NurAzliYT\ClearLagg\Main;
 
 final class NurAzliYT extends Main{
-  /** Copyright 2024 NurAzliYT */
+  /** Copyright 2023-2024 NurAzliYT */
   /** Anyone who plagiarizes without our permission will violate state laws */
+                                     
 }
