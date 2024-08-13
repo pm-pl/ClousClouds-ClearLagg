172bad9d3cc3bc3909ca4965ed4636d51b52f47c
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2024-08-13 20:20:20 +0700

    Update NurAzliYT.php

--- src/NurAzliYT/ClearLagg/NurAzliYT.php
+++ src/NurAzliYT/ClearLagg/NurAzliYT.php
@@ -7,5 +7,497 @@ use NurAzliYT\ClearLagg\Main;
 final class NurAzliYT extends Main{
   /** Copyright 2023-2024 NurAzliYT */
   /** Anyone who plagiarizes without our permission will violate state laws */
-                                     
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+ /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+/**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+/**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  /**
+ * Plugin Created By NurAzliYT https://github.com/NurAzliYT
+ *    ____ _                 _                      
+ *  / ___| | ___  __ _ _ __| |    __ _  __ _  __ _ 
+ * | |   | |/ _ \/ _` | '__| |   / _` |/ _` |/ _` |
+ * | |___| |  __/ (_| | |  | |__| (_| | (_| | (_| |
+ *  \____|_|\___|\__,_|_|  |_____\__,_|\__, |\__, |
+ *                                     |___/ |___/ 
+ * License LGPL-4
+ * NurAzliYT Team
+ * https://xpocketmc.xyz
+ */
+  
 }
