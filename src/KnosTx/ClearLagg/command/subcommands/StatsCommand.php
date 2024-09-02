03948266be246bf65eaf9d831c96808c6df86135
KnosTx <135945845+KnosTx@users.noreply.github.com>
2024-09-02 21:05:49 +0700

    Update StatsCommand.php

--- src/KnosTx/ClearLagg/command/subcommands/StatsCommand.php
+++ src/KnosTx/ClearLagg/command/subcommands/StatsCommand.php
@@ -2,33 +2,20 @@
 
 namespace KnosTx\ClearLagg\command\subcommands;
 
-use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\plugin\PluginOwned;
-use pocketmine\plugin\PluginOwnedTrait;
+use pocketmine\utils\TextFormat;
 use KnosTx\ClearLagg\Main;
 
-class StatsCommand extends Command implements PluginOwned {
-    use PluginOwnedTrait;
+class StatsCommand {
 
     private $plugin;
 
     public function __construct(Main $plugin) {
-        parent::__construct("clearlaggstats", "View ClearLagg statistics", "/clearlagg stats", []);
         $this->plugin = $plugin;
-        $this->setPermission("clearlagg.stats.command");
     }
 
-    public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
-        if (!$this->testPermission($sender)) {
-            return false;
-        }
-
-        $this->plugin->getStatsManager()->sendStats($sender);
-        return true;
-    }
-
-    public function getOwningPlugin(): Main {
-        return $this->plugin;
+    public function execute(CommandSender $sender): void {
+        $itemsCleared = $this->plugin->getStatsManager()->getItemsCleared();
+        $sender->sendMessage(TextFormat::GREEN . "Total items cleared: " . $itemsCleared);
     }
 }
